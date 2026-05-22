766d2037150ba1f07d2370c855d785ca38ba5164
nawa316 <muhammadadedzakwan@gmail.com>
2026-05-22 11:14:36 +0700

    feat: intgeration authtentication

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,21 @@
-import { PrismaClient } from '../app/generated/prisma';
+import { PrismaClient } from '../app/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+const dbUser = process.env.DB_USER || 'postgres';
+const dbPass = process.env.DB_PASS || 'postgres';
+const dbHost = process.env.DB_HOST || 'localhost';
+const dbPort = process.env.DB_PORT || '5432';
+const dbName = process.env.DB_NAME || 'amwal_db';
+const connectionString = process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?schema=public`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
